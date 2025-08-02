ecd7b5fdf66e74c1e7cf1ecee671869ae8a972d8
mmaripilrucan <mmaripilrucan@byupathway.edu>
2025-08-02 15:53:25 -0300

    Mindfulness Program

--- week05/Mindfulness/ListingActivity.cs
+++ week05/Mindfulness/ListingActivity.cs
@@ -9,7 +9,7 @@ namespace MindfulnessApp
         private List<string> _prompts = new List<string>
         {
             "Who are people that you appreciate?",
-            "What are personal strengths of yours?",
+            "What are some personal strengths of yours?",
             "Who are people that you have helped this week?",
             "When have you felt the Holy Ghost this month?",
             "Who are some of your personal heroes?"
